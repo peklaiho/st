25436749acba061866dddd01c9af72e6c6f89d11
Pekka Laiho <pekka.i.laiho@gmail.com>
2022-04-18 09:00:47 +0700

    use size instead of pixelsize in font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Droid Sans Mono:pixelsize=14:antialias=true:autohint=true";
+static char *font = "Droid Sans Mono:size=11:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
