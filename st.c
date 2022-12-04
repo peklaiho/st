499911006971cb2c4bc5f82850e468c82fa37fd6
Pekka Laiho <pekka.i.laiho@gmail.com>
2022-12-04 13:00:45 +0700

    larger history size for scrollback

--- st.c
+++ st.c
@@ -35,7 +35,7 @@
 #define ESC_ARG_SIZ   16
 #define STR_BUF_SIZ   ESC_BUF_SIZ
 #define STR_ARG_SIZ   ESC_ARG_SIZ
-#define HISTSIZE      5000
+#define HISTSIZE      50000
 
 /* macros */
 #define IS_SET(flag)		((term.mode & (flag)) != 0)
